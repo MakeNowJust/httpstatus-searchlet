15316486e47118fa8dffb9c745d6bb7dec876ee6
MakeNowJust <make.just.on@gmail.com>
2014-03-07 21:11:55 +0900

    create a build system

---
+++ build.js
@@ -0,0 +1,66 @@
+//load require libraries
+
+var
+fs     = require('fs'),
+_      = require('lodash'),
+async  = require('async'),
+uglify = require('uglify-js');
+
+//constants
+
+var
+VERSION       = '0.0.1',
+HTTPSTATUS_JS = 'httpstatus.js';
+
+//processing command line arguments
+
+var
+optimist = require('optimist')
+  .usage   ('Usage:\n  > node build.js [options]')
+  .default ('i', '"%s"')
+  .alias   ('i', 'input-pattern')
+  .describe('i', 'string to replace $input_pattern$')
+  .default ('o', 'httpstatus.min.js')
+  .alias   ('o', 'output')
+  .describe('o', 'output filename'),
+argv = optimist.argv;
+
+if (argv.help) {
+  optimist.showHelp();
+  process.exit();
+}
+
+if (argv.version) {
+  console.error(VERSION);
+  process.exit();
+}
+
+async.waterfall([
+  function loadHttpstatusJs(next) {
+    fs.readFile(HTTPSTATUS_JS, 'utf-8', next);
+  },
+  
+  function compressHttpstatusJs(src, next) {
+    var
+    ast = uglify.parse(src);
+    
+    ast.figure_out_scope();
+    ast.compute_char_frequency();
+    ast.mangle_names();
+    
+    next(null, ast.print_to_string());
+  },
+  
+  function replaceHttpstatusJs(compressedSrc, next) {
+    compressedSrc = compressedSrc.replace('$input_pattern$', argv['input-pattern']);
+    next(null, compressedSrc);
+  },
+  
+  function writeHttpstatusJs(replacedSrc, next) {
+    replacedSrc = 'javascript:' + replacedSrc;
+    fs.writeFile(argv.output, replacedSrc, 'utf-8', next);
+  },
+], function (err) {
+  if (err) throw err;
+  console.log('success generate => %s', argv.output);
+});
